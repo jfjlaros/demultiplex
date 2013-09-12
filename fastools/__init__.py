1d2e98287118de75fdd8d293d9b4b385025a3e6d
Jeroen F.J. Laros <J.F.J.Laros@lumc.nl>
2013-09-12 11:57:25 +0000

    Added a reverse-complement function.

--- fastools/__init__.py
+++ fastools/__init__.py
@@ -23,7 +23,7 @@ Licensed under the MIT license, see the LICENSE file.
 
 RELEASE = False
 
-__version_info__ = ('0', '6', '0')
+__version_info__ = ('0', '7', '0')
 
 
 __version__ = '.'.join(__version_info__)
