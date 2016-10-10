885e6098736c9bc9052933e1ba0ae2909e62f2ac
J.F.J. Laros <J.F.J.Laros@lumc.nl>
2016-10-10 19:00:48 +0200

    Version bump.

--- fastools/__init__.py
+++ fastools/__init__.py
@@ -2,14 +2,13 @@
 fastools: Various tools for the analysis and manipulation of FASTA/FASTQ files.
 
 
-Copyright (c) 2015 Leiden University Medical Center <humgen@lumc.nl>
-Copyright (c) 2015 Jeroen F.J. Laros <J.F.J.Laros@lumc.nl>
+Copyright (c) 2016 Leiden University Medical Center <humgen@lumc.nl>
+Copyright (c) 2016 Jeroen F.J. Laros <J.F.J.Laros@lumc.nl>
 
 Licensed under the MIT license, see the LICENSE file.
 """
 
-__version_info__ = ('0', '13', '1')
-
+__version_info__ = ('0', '14', '0')
 
 __version__ = '.'.join(__version_info__)
 __author__ = 'LUMC, Jeroen F.J. Laros'
@@ -18,8 +17,10 @@ __homepage__ = 'https://git.lumc.nl/j.f.j.laros/fastools'
 
 usage = __doc__.split("\n\n\n")
 
+
 def doc_split(func):
     return func.__doc__.split("\n\n")[0]
+
 
 def version(name):
     return "%s version %s\n\nAuthor   : %s <%s>\nHomepage : %s" % (name,
