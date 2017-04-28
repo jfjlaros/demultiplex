2b40c46da963ddcaed557150a1f3b4b72842fc0b
J.F.J. Laros <J.F.J.Laros@lumc.nl>
2017-04-28 13:00:53 +0200

    Fixed dependency issue.

--- fastools/__init__.py
+++ fastools/__init__.py
@@ -7,7 +7,7 @@ Copyright (c) 2016 Jeroen F.J. Laros <J.F.J.Laros@lumc.nl>
 
 Licensed under the MIT license, see the LICENSE file.
 """
-__version_info__ = ('0', '14', '0')
+__version_info__ = ('0', '14', '1')
 
 __version__ = '.'.join(__version_info__)
 __author__ = 'LUMC, Jeroen F.J. Laros'
